1de28b1a9abbfa49afac0fbdcad1c5fab16f32d6
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2020-11-30 14:16:49 +0530

    Added guess.py
    Simple guessing game

---
+++ guess.py
@@ -0,0 +1,26 @@
+#Got Bored, So Created this game using Random Module
+
+import random
+
+snum = random.randint(1,51)
+
+def guess():
+  for x in range(8):
+    guess = int(input("Enter your guess.."))
+    if guess < snum:
+      print("Your guess is too low.")
+    elif guess > snum:
+      print("Your guess is too high")
+    else:
+      break
+  
+  if guess == snum:
+    print("Noice!, your guessed my number in {} guesses!".format(x))
+  else:
+    print("Nope. It was {}".format(snum))
+
+
+#Fuction Call.
+guess()
+
+
